373467e3cac7862234922383cc77431bf26c6f77
ankitaa0926573-wq <ankitaa.0926573@gmail.com>
2026-09-08 21:10:09 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* LIS NET Hub - offline cache. Bump CACHE when you redeploy. */
-const CACHE = 'lisnet-2026-09-08';
+const CACHE = 'lisnet-2026-09-08-2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest',
                 './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 
