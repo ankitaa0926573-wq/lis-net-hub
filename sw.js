62506662fa815aa9494e9a278250537b7e640cc9
ankitaa0926573-wq <ankitaa.0926573@gmail.com>
2026-09-22 22:45:09 +0530

    hy

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* LIS NET Hub - offline cache. Bump CACHE when you redeploy. */
-const CACHE = 'lisnet-2026-09-18-2';
+const CACHE = 'lisnet-2026-09-22-2';
 const ASSETS = ['./', './index.html', './manifest.webmanifest',
                 './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 
